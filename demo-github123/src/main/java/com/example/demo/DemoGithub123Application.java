c5c76b71a2669f0bc5ae07383394c1d117e00bfa
Komal1976 <komalghodeswar86@gmail.com>
2022-07-25 23:04:02 +0530

    spring boot project done in github

---
+++ demo-github123/src/main/java/com/example/demo/DemoGithub123Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithub123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithub123Application.class, args);
+	}
+
+}
